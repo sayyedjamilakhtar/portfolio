b9e09678d18d7441b7be8bbea89cd5b9b9c770f0
sayyedjamilakhtar <sayyedjamilakhtar@gmail.com>
2025-12-17 01:14:20 +0530

    Created Navbar

--- components/common/SmoothScroll.tsx
+++ components/common/SmoothScroll.tsx
@@ -1,18 +1,22 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, type ReactNode } from "react";
 import Lenis from "lenis";
 
-export default function SmoothScroll({ children }) {
+interface SmoothScrollProps {
+  children: ReactNode;
+}
+
+export default function SmoothScroll({ children }: SmoothScrollProps) {
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
-      smoothTouch: false,
+      // smoothTouch: false,
     });
 
-    function raf(time) {
+    function raf(time: number) {
       lenis.raf(time);
       requestAnimationFrame(raf);
     }
